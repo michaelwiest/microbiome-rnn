7c0029a5e673ee8066bd17ec512c12cdf2fb97e1
Michael Wiest <michaelewiest@gmail.com>
2018-04-25 14:00:40 -0700

    Helper cleanup. Also PCA for hidden layers.

--- helpers/otu_matrix_completer.py
+++ helpers/otu_matrix_completer.py
@@ -2,11 +2,16 @@ from DEICODE import untangle
 import sys
 import pandas as pd
 
-fname = sys.argv[1]
-df = pd.read_csv(fname, index_col=0)
-# df = df.T
-completed = pd.DataFrame(untangle.complete_matrix(df.as_matrix().copy(),
-                                                  iteration=100, minval=0.1),
-                         index=df.index, columns=df.columns)
-out_name = ''.join(fname.split('.')[:-1]) + '_completed.csv'
-df.to_csv(out_name)
+
+def main():
+    fname = sys.argv[1]
+    df = pd.read_csv(fname, index_col=0)
+    # df = df.T
+    completed = pd.DataFrame(untangle.complete_matrix(df.as_matrix().copy(),
+                                                      iteration=100, minval=0.1),
+                             index=df.index, columns=df.columns)
+    out_name = ''.join(fname.split('.')[:-1]) + '_completed.csv'
+    completed.to_csv(out_name)
+
+if __name__ == '__main__':
+    main()
