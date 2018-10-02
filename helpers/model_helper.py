a3ac84a9fb210cb9db04d083bd0564967b3fac9c
Michael Wiest <michaelewiest@gmail.com>
2018-10-02 11:44:35 -0700

    Fixed requires grad

--- helpers/model_helper.py
+++ helpers/model_helper.py
@@ -4,13 +4,13 @@ from torch.autograd import Variable
 import torch
 
 def add_cuda_to_variable(data_nums, is_gpu, requires_grad=True):
-    tensor = torch.FloatTensor(data_nums, requires_grad=requires_grad)
+    tensor = torch.FloatTensor(data_nums)
     if len(tensor.size()) == 1:
         tensor = tensor.unsqueeze_(0).unsqueeze(0).transpose(0, 2)
     else:
         pass
         # tensor = tensor.unsqueeze_(len(tensor.size()))
     if is_gpu:
-        return Variable(tensor.cuda())
+        return Variable(tensor.cuda(), requires_grad=requires_grad)
     else:
-        return Variable(tensor)
+        return Variable(tensor, requires_grad=requires_grad)
