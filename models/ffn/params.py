a509f127bfdf8aef47a192b5328ec6650d2f72f6
Michael Wiest <michaelewiest@gmail.com>
2018-10-12 11:20:20 -0700

    param cleanup

--- models/ffn/params.py
+++ models/ffn/params.py
@@ -8,11 +8,8 @@ learning_rate = 0.0000005
 # Length of the sequence being passed to the LSTM or FFN.
 seq_len = 25
 # For the compression step with the LSTM how many strains to reduce it to.
-reduced_num_strains = 80
-# If this value is below one then it increases by that percent
-# every epoch. If greater than one then it increases by that fixed
-# amount after every epoch. Only used for RNN.
-slice_incr_amt = 1
+reduced_num_strains = 25
+
 
 # Use conv
 use_convs = True
