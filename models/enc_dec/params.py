b27870072670458e81c8db7a1f2d4121afbc2a8b
Michael Wiest <michaelewiest@gmail.com>
2018-10-25 10:30:56 -0700

    better params

--- models/enc_dec/params.py
+++ models/enc_dec/params.py
@@ -1,18 +1,18 @@
 batch_size = 50
 # Size of the hidden layers of the network.
-hidden_dim = 128
+hidden_dim = 256
 # How many samples to check before one epoch. Kind of arbitrary.
 samples_per_epoch = 500000
 num_epochs = 50
 learning_rate = 0.0000005
 # Length of the sequence being passed to the LSTM or FFN.
-seq_len = 15
+seq_len = 5
 # For the compression step with the LSTM how many strains to reduce it to.
-reduced_num_strains = 25
+reduced_num_strains = 62
 # If this value is below one then it increases by that percent
 # every epoch. If greater than one then it increases by that fixed
 # amount after every epoch. Only used for RNN.
-slice_incr_frequency = None
+slice_incr_frequency = 2
 
 # What percentage of the time to use teacher forcing during training
 teacher_force_frac = 0.5
