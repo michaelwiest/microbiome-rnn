1edde267feadf5334123f79eadddef17afdd9baa
Michael Wiest <michaelewiest@gmail.com>
2019-01-10 09:46:01 -0800

    reduced number of strains in input linear step

--- dev/models/enc_dec/params.py
+++ dev/models/enc_dec/params.py
@@ -9,7 +9,7 @@ weight_decay = 0
 # Length of the sequence being passed to the LSTM or FFN.
 seq_len = 5
 # For the compression step with the LSTM how many strains to reduce it to.
-reduced_num_strains = 62
+reduced_num_strains = 31
 # If this value is below one then it increases by that percent
 # every epoch. If greater than one then it increases by that fixed
 # amount after every epoch. Only used for RNN.
