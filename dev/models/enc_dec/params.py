77a9e15dd0039c82bdae937712aa1b8eb1507c7b
Michael Wiest <michaelewiest@gmail.com>
2019-01-10 10:45:28 -0800

    fixed name

--- dev/models/enc_dec/params.py
+++ dev/models/enc_dec/params.py
@@ -30,7 +30,7 @@ output_dir = 'enc_dec_output'
 use_early_stopping = True
 early_stopping_patience = 15
 
-additional_comments = '_swapped_test'
+additional_comments = ''
 
 run_suffix = '_EncDec_h{}_sl{}_rns{}_sif{}_numlstm{}{}'.format(hidden_dim,
                                               seq_len,
